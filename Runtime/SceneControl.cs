1dd81d88b018aaa483a86e23852bb3cf00653048
Ming <68538756+mingowo@users.noreply.github.com>
2024-11-07 11:05:44 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace MING.Tools {
+    /// <summary>
+    /// 場景控制, 提供載入場景與退出遊戲功能
+    /// </summary>
+    public class SceneControl : MonoBehaviour {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName">要載入的場景名稱</param>
+        public void LoadScene(string sceneName) {
+            //場景管理器 的 載入場景(場景名稱)
+            SceneManager.LoadScene(sceneName);
+        }
+        /// <summary>
+        /// 退出遊戲
+        /// </summary>
+        public void QuitGame() {
+            //應用程式的離開();
+            Application.Quit();
+        }
+    }
+}
